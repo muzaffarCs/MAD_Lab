274095f83731cf8001b642fc63cc999568ee5e3a
muzaffarCs <127599404+muzaffarCs@users.noreply.github.com>
2026-09-11 19:08:37 +0500

    fixed the issue generated while managing the github main Repo

--- first_Lab/android/app/build.gradle.kts
+++ first_Lab/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_first_lab"
+    namespace = "com.example.test_lab"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_first_lab"
+        applicationId = "com.example.test_lab"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
